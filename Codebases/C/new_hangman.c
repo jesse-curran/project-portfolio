697c8564b0213960ab7eddd344c172c439e13a5b
Jesse Curran <jessecurran01@icloud.com>
2024-05-17 13:23:14 -0700

    Near working hangman product, for terminal, mvp.

--- Codebases/C/new_hangman.c
+++ Codebases/C/new_hangman.c
@@ -4,8 +4,7 @@
 #include <ctype.h>  // For character handling functions.
 #include <stdbool.h>
 
-
-// function to print hangman after each try
+// Function to print hangman after each try
 void printHangman(int t) {
     switch (t) {
         case 0:
@@ -72,82 +71,121 @@ void printHangman(int t) {
                    "=========\n");
             break;
         default:
-            printf("Invalid number of tries.\n");
+            printf("Invalid number of attempts...\n");
             break;
     }
 }
 
-// Function to print greeting message
-void greetingMsg() {
-    printf("Welcome to the game of HANGMAN!\n");
-    printf("You will be prompted to input a guess.\n");
-    printf("etc ...\n");
-}
-
-// Function to print farewell message
-void farewellMsg(bool result) {
-    if (result == 1) {
-        printf("Congrats on your victory! Goodbye :)");
-    } else {
-        printf("Ooof, not so hot. The man no more. Play Again?");
-    }
-}
-
-// Static function to print underscores for words to go above
-void printUnderscores(int wordCount) {
-    int i;
-    for (i = 0; i < wordCount; i++) {
-        printf(" _ ");
+void printWordProgress(int wordCount, char guess, int position, bool guessResult, int tryNumber, char *userAnswer) {
+    
+    
+    for (int i = 0; i < wordCount; i++) {
+        if (tryNumber == 0) {
+            printf(" _ ");
+        } else if (userAnswer[i] != '_') {
+            printf(" %c ", userAnswer[i]);
+        } else {
+            printf(" _ ");
+        }
     }
     printf("\n");
 }
 
-char getUserGuess() {
-    char guess;
-    printf("Type a letter to guess: \n");
-    scanf("%c", &guess);
-    printf("Your guess was: '%c'", guess);
+// Function to print greeting message
+void greetingMsg() {
+    printf("\n\n\nWelcome to the game of HANGMAN!\n");
+    printf("You will be prompted to input a guess.\n");
+    printf("etc ...\n\n\n");
 }
 
-bool calcGuessResult(char guess, answer) {
-    // iterate over word, check guess char to each word, return true or false
-    bool result;
-    for (i = 0; i < 5; i++) {
-        if ()
+// Function to print farewell message
+void farewellMsg(bool result) {
+    if (result) {
+        printf("WAHOOO !!! Congrats on your victory! Goodbye :)\n");
+    } else {
+        printf("NOOOOOOOOO. You lost :( \nBetter luck next time.");
     }
+}
+
+char getUserGuess() {
+    char guess;
+    printf("\nType an UPPERCASE letter to guess: \n");
+    scanf(" %c", &guess);
+    printf("Your guess was: '%c'\n", guess);
+    return guess;
+}
+
+// Function to check if the guess is in the word and update the position
+bool checkGuess(char guess, char *answer, int *position) {
+    for (int i = 0; i < strlen(answer); i++) {
+        if (answer[i] == guess) {
+            *position = i;
+            return true;
+        }
+    }
+    return false;
 }
 
 // Drive the game, calling functions
 int main() {
-    int t = 1;
-    bool currResult;
-    bool endResult;
+    int t = 0;
     int wordCount = 5;
     char guess;
     bool guessResult;
-    char answer[] = "Irate";
+    char answer[] = "STUMP";
+    bool playing = true;
+    bool gameResult = false;
+    int position;
+    char userAnswer[wordCount + 1];
+    memset(userAnswer, '_', wordCount);
+    userAnswer[wordCount] = '\0';
 
     greetingMsg();
 
-    while (t <= 6) {
+    // play hangman
 
-        if (t == 1) { // when first guess, print blank game above
-            printHangman(0);               // or when guess was true
-            printUnderscores(wordCount);
+    // begin with showing user blank game canvas
+    printHangman(t);
+    //printWordProgress(wordCount, guess, position, guessResult, t, userAnswer);
+    printf("%s\n", userAnswer);
+
+    while (playing) {
+        guess = getUserGuess();
+
+        printf("Calculating if your guess was correct ...\n\n");
+
+        guessResult = checkGuess(guess, answer, &position);
+
+        if (guessResult) {
+            printf("Woah, CORRECT GUESS!\n\n");
+
+        } else {
+            printf("Uh oh, INCORRECT GUESS.\n\n");
         }
 
-        guess = getUserGuess(); // for ex, guess = 'A'
-        guessResult = calcGuessResult(guess, answer);
-        
-        printHangman(t);
 
-        printUnderscores(wordCount);
-        t++;
+        printf("Printing the updated game board ... \n\n");
+
+        if (!guessResult) { // incorrect guess
+            t++;
+            printHangman(t);
+            //printWordProgress(wordCount, guess, position, guessResult, t, userAnswer);
+            printf("%s\n", userAnswer);
+        } else {    // correct guess
+            userAnswer[position] = guess;
+            //printf("User answer text array is now %s\n", userAnswer);
+            printHangman(t);
+            //printWordProgress(wordCount, guess, position, guessResult, t, userAnswer);
+            printf("%s\n", userAnswer);
+        }
+
+        if (t == 6 || strcmp(userAnswer, answer) == 0) {
+            playing = false;    // end game when 6 tries or word guessed
+            gameResult = (strcmp(userAnswer, answer) == 0);
+        }
     }
 
-    farewellMsg(endResult);
+    farewellMsg(gameResult);
 
     return 0;
 }
-
-
